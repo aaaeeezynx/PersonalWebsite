a62a567cc0fa49a0a1d593c1d13d5acf94d888f9
aaaeeezynx <aaaeee33@gmil.com>
2025-05-11 05:07:42 +0800

    use Owl Carousel show blog card

--- index.js
+++ index.js
@@ -78,3 +78,17 @@ contentArray.forEach(item => {
 
     contentList.appendChild(li);
 });
+
+$('.owl-carousel').owlCarousel({
+    items: 3, // 一次顯示三張卡片
+    loop: true, // 無限循環
+    margin: 10, // 卡片間距
+    nav: true, // 顯示導航箭頭
+    dots: true, // 顯示導航點
+    slideBy: 1, // 每次滑動一張
+    responsive: {
+        0: { items: 1 }, // 行動裝置顯示一張
+        768: { items: 2 }, // 平板顯示兩張
+        992: { items: 3 } // 桌面顯示三張
+    }
+});
